e7dddf7beed0a86cf4ee28edb8b45f2aeefcf0a8
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-09-16 05:29:13 +0530

    wispay css

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,48 +2,67 @@
 import "./hero.css";
 import DiamongSvg from "../assets/diamond-large-round.png";
 import AppleIcon from "../assets/apple.png";
-import Walleticon from "../assets/wallet.png";
 import Wishpay from "../assets/wishpay.avif";
 const HeroSection = () => {
   return (
-    <div id='main-container' className='mt-10'>
+    <div>
+      <div id="main-container" className="mt-10">
 
-      <div id="left-section" >
-        <img src={DiamongSvg.toString()} alt="Diamond" id='purple' />
-        <img src={AppleIcon.toString()} alt="Coin" id='coin' />
+        {/* Left Section */}
+        <div id="left-section">
+          <img src={DiamongSvg.toString()} alt="Diamond" id="purple" />
+          <img src={AppleIcon.toString()} alt="Coin" id="coin" />
+        </div>
+
+        {/* Middle Section */}
+        <div id="middle-section">
+          <h1>
+            <span id="enhance">Enhance</span>
+            <span>
+              <img src={Wishpay.toString()} alt="icons" className="h-[50px] w-[50px]" id="wish" />
+            </span>
+            <span id="your">Your</span>
+            <br />
+            <span id="finance">Financials with Wishpay</span>
+          </h1>
+        </div>
 
 
-
-
-
+        <div id="right-section" className="max-w-[300px]">
+          <img src={DiamongSvg.toString()} alt="Diamond" id="blue" />
+        </div>
       </div>
-      <div id="middle-section" >
-        <h1>
-          <span id="enhance">
-            Enhance
-
-          </span>
-          <span><img src={Wishpay.toString()} alt="icons" className="h-[50px] w-[50px]" id="wish" /></span>  <span id="your">Your</span>
-          <br />
 
 
-          <span id="finance">Financials with Wishpay</span>
-        </h1>
-
+      <div className="hero-bottom">
+        <p>
+          Moving money should never take more than few <br />
+          taps. Transfers are always free between friends
+        </p>
+        <button id="send-button">Send Money Now</button>
       </div>
 
 
-      <div className=" max-w-[300px]" id="right-section" >
-        <img src={DiamongSvg.toString()} alt="Diamond" id="blue" />
+      <div className="stats-container">
+        <div className="stat">
+          <p className="stat-number">75K+</p>
+          <p className="stat-label">Satisfied users globally</p>
+        
+        </div>
+        <div className="logos">
+            <img src="https://1000logos.net/wp-content/uploads/2024/07/OpenAI-Logo-2022.png" alt="OpenAI" />
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqRrf_EZ0AKLn8XHzM5o-B-rPk1GVneGnkKA&s" alt="Raycast" />
+            <img src="https://wingassistant.com/wp-content/uploads/2022/07/ramp-logo.png" alt="Ramp" />
+          </div>
 
-
-
-
-
+        <div className="stat">
+          <p className="stat-number">92%</p>
+          <p className="stat-label">Beneficial User Cashback</p>
+        </div>
       </div>
 
     </div>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
